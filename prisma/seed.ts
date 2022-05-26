a43f06cfd9ec2e54a4cb180c91009d9a009802da
Tim Shaker <tim@tmshkr.com>
2022-05-25 18:23:01 -0700

    add seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,7 +9,6 @@ const prisma: PrismaClient = new PrismaClient();
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
 });
 
