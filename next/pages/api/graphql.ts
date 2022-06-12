5bd20c3e217061a4acaef7b47d81816d7a15e4ce
Tim Shaker <tim@tmshkr.com>
2022-06-12 01:49:36 -0700

    add resolvers

--- next/pages/api/graphql.ts
+++ next/pages/api/graphql.ts
@@ -4,17 +4,42 @@ import prisma from "lib/prisma";
 const typeDefs = /* GraphQL */ `
   type Query {
     users: [User!]!
+    projects: [Project!]
+    meetings: [Meeting!]
   }
   type User {
     slack_id: String
+  }
+  type Project {
+    id: ID
+    created_by: String
+    is_active: Boolean
+    name: String
+  }
+
+  type Meeting {
+    id: ID
+    created_by: String
+    duration: Int
+    is_active: Boolean
+    project_id: ID
+    rrule: String
+    slack_channel_id: String
+    start_date: String
+    title: String
   }
 `;
 
 const resolvers = {
   Query: {
-    async users(parent, args, context) {
-      const users = await prisma.user.findMany({ where: {} });
-      return users;
+    users(parent, args, context) {
+      return prisma.user.findMany({ where: {} });
+    },
+    projects(parent, args, context) {
+      return prisma.project.findMany({ where: {} });
+    },
+    meetings(parent, args, context) {
+      return prisma.meeting.findMany({ where: {} });
     },
   },
 };
