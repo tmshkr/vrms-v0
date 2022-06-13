725ae2ef1797c12ff6c2d5b70b19d624c9d235a4
Tim Shaker <tim@tmshkr.com>
2022-06-12 17:44:16 -0700

    add user to db on team_join

--- slackbot/src/app/events.ts
+++ slackbot/src/app/events.ts
@@ -1,5 +1,6 @@
 import { app } from "app";
 import { getHomeTab } from "./views/home";
+import prisma from "lib/prisma";
 
 export const registerEvents = () => {
   app.event("app_home_opened", async ({ event, client, logger }) => {
@@ -10,6 +11,22 @@ export const registerEvents = () => {
       logger.info(result);
     } catch (error) {
       logger.error(error);
+    }
+  });
+  app.event("team_join", async ({ event, client, logger }) => {
+    const { user } = event;
+    if (!user.is_bot) {
+      const row = {
+        slack_id: user.id,
+        real_name: user.real_name,
+        email: user.profile.email,
+      };
+      await prisma.user.upsert({
+        where: { slack_id: user.id },
+        update: row,
+        create: row,
+      });
+      console.log(`added user ${user.id}`);
     }
   });
 
