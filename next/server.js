880be00aab27da4742bb3c0c8b493f4cdcda1de4
Tim Shaker <tim@tmshkr.com>
2022-06-14 11:33:15 -0700

    https dev server

---
+++ next/server.js
@@ -0,0 +1,24 @@
+/*
+https://medium.com/@greg.farrow1/nextjs-https-for-a-local-dev-server-98bb441eabd7
+*/
+const { createServer } = require("https");
+const { parse } = require("url");
+const next = require("next");
+const fs = require("fs");
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const httpsOptions = {
+  key: fs.readFileSync("./certificates/localhost.key"),
+  cert: fs.readFileSync("./certificates/localhost.crt"),
+};
+
+app.prepare().then(() => {
+  createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(3000, (err) => {
+    if (err) throw err;
+    console.log("> Server started on https://localhost:3000");
+  });
+});
