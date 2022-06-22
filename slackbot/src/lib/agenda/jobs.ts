933d85b2bdd6750ffbaf9338d7fbb212faa46610
Tim Shaker <tim@tmshkr.com>
2022-06-22 16:53:10 -0700

    update sendMeetingCheckin

--- slackbot/src/lib/agenda/jobs.ts
+++ slackbot/src/lib/agenda/jobs.ts
@@ -1,10 +1,16 @@
+import prisma from "lib/prisma";
 import { sendMeetingCheckin } from "app/notifications";
 import { getNextOccurrence } from "lib/rrule";
 
 export function registerJobs(agenda) {
   agenda.define("sendMeetingCheckin", async (job) => {
-    const { rrule, slack_channel_id } = job.attrs.data;
-    await sendMeetingCheckin(slack_channel_id);
+    const { meeting_id } = job.attrs.data;
+    const { slack_channel_id, rrule, google_meet_link } =
+      await prisma.meeting.findUnique({
+        where: { id: meeting_id },
+      });
+    await sendMeetingCheckin(slack_channel_id, google_meet_link);
+
     if (rrule) {
       const nextRunAt = getNextOccurrence(rrule);
       job.schedule(nextRunAt);
