bb5580a1e5cad489eda7fc20a65392a8a17a5c13
Tim Shaker <tim@tmshkr.com>
2022-08-24 02:14:30 -0700

    navigation

--- next/components/Dashboard.tsx
+++ next/components/Dashboard.tsx
@@ -1,8 +1,10 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import { getPathRoot } from "utils/path";
 
 const user = {
   name: "Tom Cook",
@@ -10,12 +12,7 @@ const user = {
   imageUrl:
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
-const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-];
+
 const userNavigation = [
   { name: "Your Profile", href: "#" },
   { name: "Settings", href: "#" },
@@ -26,18 +23,19 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export function Dashboard() {
+export function Dashboard({ children }) {
+  const router = useRouter();
   const { data: session, status } = useSession();
+  const pathRoot = getPathRoot(router.pathname);
+  const navigation = [
+    { name: "Dashboard", href: "/", current: pathRoot === "/" },
+    { name: "Meetings", href: "/meetings", current: pathRoot === "/meetings" },
+    { name: "Projects", href: "/projects", current: pathRoot === "/projects" },
+    { name: "Calendar", href: "/calendar", current: pathRoot === "/calendar" },
+  ];
+
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full">
-        <body class="h-full">
-        ```
-      */}
       <div className="min-h-full">
         <Disclosure as="nav" className="bg-white border-b border-gray-200">
           {({ open }) => (
@@ -224,19 +222,10 @@ export function Dashboard() {
         </Disclosure>
 
         <div className="py-10">
-          <header>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <h1 className="text-3xl tracking-tight font-bold leading-tight text-gray-900">
-                Dashboard
-              </h1>
-            </div>
-          </header>
           <main>
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
               {/* Replace with your content */}
-              <div className="px-4 py-8 sm:px-0">
-                <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-              </div>
+              <div className="px-4 py-8 sm:px-0">{children}</div>
               {/* /End replace */}
             </div>
           </main>
