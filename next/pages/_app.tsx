885194520f708a444cec128716d7a96b0d4745f5
Tim Shaker <tim@tmshkr.com>
2022-08-26 10:20:37 -0700

    return null on redirect

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -13,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
     if (redirect_to) {
       Cookies.remove("redirect_to");
       window.location.replace(redirect_to);
+      return null;
     }
   }
 
