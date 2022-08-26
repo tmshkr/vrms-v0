a5afa607b8a51a6bcd80687dfa81481b88747efb
Tim Shaker <tim@tmshkr.com>
2022-08-26 13:35:19 -0700

    auth routing

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,12 +1,22 @@
 import "../styles/globals.scss";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { useSession } from "next-auth/react";
 import { Dashboard } from "components/Dashboard";
 import Cookies from "js-cookie";
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & {
+  Component: {
+    auth: {
+      role?: string;
+    };
+  };
+}) {
   const cookies = Cookies.get();
   if (cookies) {
     const { redirect_to } = cookies;
@@ -20,8 +30,25 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session} refetchInterval={0}>
       <Dashboard>
-        <Component {...pageProps} />
+        {Component.auth ? (
+          <Auth>
+            <Component {...pageProps} />
+          </Auth>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </Dashboard>
     </SessionProvider>
   );
+}
+
+function Auth({ children }) {
+  // if `{ required: true }` is supplied, `status` can only be "loading" or "authenticated"
+  const { status } = useSession({ required: true });
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  return children;
 }
