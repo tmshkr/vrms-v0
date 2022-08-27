69635e36db5b5dfa3bf7237acc3fa0dae931e469
Tim Shaker <tim@tmshkr.com>
2022-08-26 19:06:42 -0700

    store roles in cookie

--- next/pages/api/auth/[...nextauth].js
+++ next/pages/api/auth/[...nextauth].js
@@ -2,67 +2,103 @@ import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import prisma from "lib/prisma";
 import { getMongoClient } from "~/lib/mongo";
+const Cookies = require("cookies");
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 
-export default NextAuth({
-  // https://next-auth.js.org/configuration/providers/oauth
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
-  theme: {
-    colorScheme: "light",
-  },
-  callbacks: {
-    async signIn(args) {
-      return true;
-    },
-    async redirect({ url, baseUrl }) {
-      return baseUrl;
-    },
-    async session(args) {
-      // console.log("session", args);
-      const { session, token, user } = args;
-      session.user.gh_username = token.gh_username;
-      return session;
-    },
-    async jwt(args) {
-      // console.log("jwt", args);
-      const { token, user, account, profile } = args;
+const nextAuthOptions = (req, res) => {
+  const cookies = new Cookies(req, res, {
+    keys: process.env.COOKIE_KEYS.split(" "),
+  });
 
-      // account and profile are available during sign in
-      if (account && profile) {
-        const { provider, providerAccountId } = account;
-        const { login, two_factor_authentication } = profile;
+  return {
+    providers: [
+      GitHubProvider({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
+      }),
+    ],
+    theme: {
+      colorScheme: "light",
+    },
+    callbacks: {
+      async signIn(args) {
+        return true;
+      },
+      async redirect({ url, baseUrl }) {
+        return baseUrl;
+      },
+      async session(args) {
+        const { session, token } = args;
+        session.user.gh_username = token.gh_username;
+        session.user.two_factor_authentication =
+          token.two_factor_authentication;
 
-        token.provider = provider;
-        token.providerAccountId = providerAccountId;
-        token.gh_username = login;
-        token.two_factor_authentication = two_factor_authentication;
-
-        const mongoClient = await getMongoClient();
-        await mongoClient
-          .db()
-          .collection("accounts")
-          .updateOne(
-            { provider, providerAccountId },
-            {
-              $set: {
-                ...user,
-                ...account,
-                gh_username: login,
-                two_factor_authentication,
+        // store the roles in a cookie to prevent excessive db queries
+        let app_roles = cookies.get("app_roles", { signed: true });
+        if (!app_roles) {
+          app_roles = await prisma.user
+            .findUnique({
+              where: {
+                gh_account_id: Number(token.providerAccountId),
               },
-            },
-            { upsert: true }
-          );
-      }
+              select: {
+                app_roles: {
+                  select: { role: true },
+                },
+              },
+            })
+            .then(({ app_roles }) =>
+              app_roles.map(({ role }) => role).join(",")
+            );
 
-      return token;
+          cookies.set("app_roles", app_roles, {
+            signed: true,
+            maxAge: 1000 * 60,
+          });
+        }
+
+        session.user.roles = app_roles;
+        return session;
+      },
+      async jwt(args) {
+        const { token, user, account, profile } = args;
+
+        // account and profile are available during sign in
+        if (account && profile) {
+          const { provider, providerAccountId } = account;
+          const { login, two_factor_authentication } = profile;
+
+          token.provider = provider;
+          token.providerAccountId = providerAccountId;
+          token.gh_username = login;
+          token.two_factor_authentication = two_factor_authentication;
+
+          const mongoClient = await getMongoClient();
+          await mongoClient
+            .db()
+            .collection("accounts")
+            .updateOne(
+              { provider, providerAccountId },
+              {
+                $set: {
+                  ...user,
+                  ...account,
+                  gh_username: login,
+                  two_factor_authentication,
+                },
+              },
+              { upsert: true }
+            );
+        }
+
+        return token;
+      },
     },
-  },
-});
+  };
+};
+
+export default (req, res) => {
+  return NextAuth(req, res, nextAuthOptions(req, res));
+};
