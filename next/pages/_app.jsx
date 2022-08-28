978ede0ece8d5c870f1015b72535d41ac74df371
Tim Shaker <tim@tmshkr.com>
2022-08-27 18:17:42 -0700

    rbac logic

--- next/pages/_app.jsx
+++ next/pages/_app.jsx
@@ -1,22 +1,12 @@
 import "../styles/globals.scss";
 import { SessionProvider } from "next-auth/react";
-import type { AppProps } from "next/app";
 import { useSession } from "next-auth/react";
 import { Dashboard } from "components/Dashboard";
 import Cookies from "js-cookie";
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
-export default function App({
-  Component,
-  pageProps,
-}: AppProps & {
-  Component: {
-    auth: {
-      role?: string;
-    };
-  };
-}) {
+export default function App({ Component, pageProps }) {
   const cookies = Cookies.get();
   if (cookies) {
     const { redirect_to } = cookies;
@@ -31,7 +21,7 @@ export default function App({
     <SessionProvider session={pageProps.session} refetchInterval={0}>
       <Dashboard>
         {Component.auth ? (
-          <Auth>
+          <Auth pageAuth={Component.auth}>
             <Component {...pageProps} />
           </Auth>
         ) : (
@@ -42,13 +32,32 @@ export default function App({
   );
 }
 
-function Auth({ children }) {
+function Auth({ pageAuth, children }) {
   // if `{ required: true }` is supplied, `status` can only be "loading" or "authenticated"
-  const { status } = useSession({ required: true });
+  const { data, status } = useSession({ required: true });
+  const { allowedRoles } = pageAuth;
 
   if (status === "loading") {
-    return <div>Loading...</div>;
+    return (
+      <div>
+        <h1 className="mt-12 text-center">Loading...</h1>
+      </div>
+    );
   }
 
-  return children;
+  const { app_roles } = data?.user.vrms_user;
+
+  for (const role of app_roles) {
+    if (allowedRoles.includes(role)) {
+      return children;
+    }
+  }
+
+  return (
+    <div>
+      <h1 className="mt-12 text-center">
+        You are not authorized to access this page
+      </h1>
+    </div>
+  );
 }
